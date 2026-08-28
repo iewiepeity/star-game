e7c048803bdb35f3c8b01b62bf3e5e3955f60919
緹緹 <96860839+iewiepeity@users.noreply.github.com>
2026-08-28 12:20:45 +0800

    refactor: remove legacy single-job data

--- src/data/job.js
+++
@@ -1,5 +0,0 @@
-// 舊版單一通告流程的相容匯出。目前先由 50 份內容庫的 J001 驅動，之後多通告狀態機可直接改用 JOB_BY_ID。
-import{JOB_BY_ID}from"./jobs-catalog.js";
-
-const source=JOB_BY_ID.J001;
-export const JOB={...source,brand:source.client};
